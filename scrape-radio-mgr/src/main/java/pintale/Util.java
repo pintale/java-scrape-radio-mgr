6a2b1d8133d5b3f9dab92c3d0db834f665999ecc
pintale <86370568+pintale@users.noreply.github.com>
2021-10-15 14:12:10 -0400

    Create Util.java

---
+++ scrape-radio-mgr/src/main/java/pintale/Util.java
@@ -0,0 +1,23 @@
+package pintale;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+public class Util
+{
+	private static Map<String,String> defaultValues=new HashMap<>() {{
+		put("directory",".");
+		put("filename","%S/%D##%A##%T");
+		put("userAgent","Apple-iPhone4C1/1001.523");
+		put("skipKnown","false");
+	}};
+
+	public static String get(String key, JsonObject object, JsonObject defaultObject)
+	{
+		JsonElement value = object.has(key)?object.get(key):(defaultObject!=null && defaultObject.has(key))?defaultObject.get(key):null;
+		return (value==null)?defaultValues.get(key):value.getAsString();
+	}
+}
